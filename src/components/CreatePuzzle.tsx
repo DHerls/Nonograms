25ed78647161d4455ce58c251bfbd75b123921d6
Dan <djherls@gmail.com>
2020-04-22 00:44:54 -0400

    Add ability to copy game key

--- src/components/CreatePuzzle.tsx
+++ src/components/CreatePuzzle.tsx
@@ -11,37 +11,71 @@ interface CreatePuzzleProps {
     onColumnsChange: (e: React.ChangeEvent) => void,
 }
 
-export class CreatePuzzle extends React.Component<CreatePuzzleProps, {}> {
-    render() {
-        return (
-          <div className="create-puzzle">
-            <form>
-              <label>
-                Number of rows:
-                <input
-                  name="row"
-                  type="number"
-                  value={this.props.rows}
-                  onChange={this.props.onRowsChange}
-                />
-              </label>
-              <label>
-                Number of columns:
-                <input
-                  name="col"
-                  type="number"
-                  value={this.props.columns}
-                  onChange={this.props.onColumnsChange}
-                />
-              </label>
-            </form>
-            <GameBoardContainer />
-            <div className="button-holder">
-                <UndoButtonContainer />
-                <ClearButtonContainer />
-            </div>
-            <p>Share this puzzle: {this.props.shareURL}</p>
-          </div>
-        );
+export const CreatePuzzle = (props: CreatePuzzleProps) => {
+
+  // 0 = waiting, 1 = success, -1 = error
+  const [copyState, setCopyState] = React.useState<number>(0);
+  
+  const onGameKeyClick = (e: React.MouseEvent<HTMLInputElement>) => {
+    e.currentTarget.select();
+    if (document.execCommand('copy')){
+      e.currentTarget.parentElement.focus();
+      setCopyState(1);
+    } else {
+      setCopyState(-1);
     }
+  }
+
+  const onMouseLeave = (e: React.MouseEvent<HTMLInputElement>) => {
+    setCopyState(0);
+  }
+
+  return (
+    <div className="create-puzzle">
+      <p>
+        Share this puzzle:{" "}
+        <span id="game-key-generated" onMouseLeave={onMouseLeave}>
+          <input
+            value={props.shareURL}
+            onClick={onGameKeyClick}
+            readOnly={true}
+          />
+          <span className={`copy-tooltip${copyState === 1 ? " copied" : ""}`}>
+            {copyState === 0
+              ? "Click to copy"
+              : copyState === 1
+              ? "Copied"
+              : "Ctrl + C to copy"}
+          </span>
+        </span>
+      </p>
+      <div className="controls">
+        <form>
+          <label>
+            Number of rows:
+            <input
+              name="row"
+              type="number"
+              value={props.rows}
+              onChange={props.onRowsChange}
+            />
+          </label>
+          <label>
+            Number of columns:
+            <input
+              name="col"
+              type="number"
+              value={props.columns}
+              onChange={props.onColumnsChange}
+            />
+          </label>
+        </form>
+        <div className="button-holder">
+          <UndoButtonContainer />
+          <ClearButtonContainer />
+        </div>
+      </div>
+      <GameBoardContainer />
+    </div>
+  );
 }
