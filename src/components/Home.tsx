d3b6a5302d26c9730283939b2f24e8fdd2de0d48
Dan <djherls@gmail.com>
2020-04-21 20:33:34 -0400

    Refactor puzzle list

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,6 +2,11 @@ import * as React from 'react';
 import { Link, useHistory } from 'react-router-dom';
 
 export const Home = ({}) => {
+    const puzzleKeys = [
+      "4:3:5:71:17:215:45:241:41:4;2:5:23:51:43:A:81:151:13:6",
+      "233:227:221:116:5222:13111323:33131212:311111212:33131212:13111323:5222:116:231:326:223;22:11:5:1111:152:2111:272:33:7:33:271:2112:152:1221:9:11:D:191:1111:191:1221:1222:231:111:1",
+    ];
+
     const [gameKey, setGameKey] = React.useState<string>("");
     const history = useHistory();
 
@@ -15,35 +20,44 @@ export const Home = ({}) => {
     }
 
     return (
-    <div className="container">
+      <div className="container">
         <div id="index">
-        <h1>Nonograms</h1>
-        <p>
-            Nonograms are logic puzzles that originated in Japan. Players fill in
-            squares to match descriptions of each row and column. When the board is
-            filled in correctly, a picture is revealed.
-        </p>
-        <h2>Pre-Made Puzzles</h2>
-        <div className="puzzle-list">
-            <Link to="/solve/4:3:5:71:17:215:45:241:41:4;2:5:23:51:43:A:81:151:13:6">
-            Puzzle 1
-            </Link>
-        </div>
-        <h2>Solve Custom Puzzles</h2>
-        <form onSubmit={onFormSubmit} id="game-key-nav">
+          <h1>Nonograms</h1>
+          <p>
+            Nonograms are logic puzzles that originated in Japan. Players fill
+            in squares to match descriptions of each row and column. When the
+            board is filled in correctly, a picture is revealed.
+          </p>
+          <h2>Pre-Made Puzzles</h2>
+          <div className="puzzle-list">
+            <ul>
+              {puzzleKeys.map((key, index) => (
+                <li key={`puzzle_${index}`}>
+                  <Link to={`/solve/${key}`}>Puzzle {index + 1}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <h2>Solve Custom Puzzles</h2>
+          <form onSubmit={onFormSubmit} id="game-key-nav">
             <label>
-            Game key:
-            <input type="text" name="gameKey" value={gameKey} onChange={onKeyChange}/>
+              Game key:
+              <input
+                type="text"
+                name="gameKey"
+                value={gameKey}
+                onChange={onKeyChange}
+              />
             </label>
             <button type="submit" className="btn btn-primary">
-            Go
+              Go
             </button>
-        </form>
-        <h2>Create Custom Puzzles</h2>
-        <Link to="/create" className="btn btn-primary">
+          </form>
+          <h2>Create Custom Puzzles</h2>
+          <Link to="/create" className="btn btn-primary">
             Create
-        </Link>
+          </Link>
         </div>
-    </div>
+      </div>
     );
 }
